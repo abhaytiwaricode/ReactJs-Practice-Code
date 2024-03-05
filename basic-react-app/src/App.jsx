d1f8d79061d0300d8676cec9b0d85bd710c3084f
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-06 00:40:13 +0530

    writing markup in jsx

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css';
 import Title from './components/Title.jsx';
 
-function Description() {
-  return <p>I am the Description!</p>;
-}
-
 function App() {
   return (
-    <>
+    <div>
       <Title />
-      <Description />
-    </>
+    </div>
   );
 }
 
