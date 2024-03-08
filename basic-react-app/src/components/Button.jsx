1337d8961de77d2b029a51202f6b5579dc9be0e9
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-08 15:35:18 +0530

    Handling Non-Click Event

--- basic-react-app/src/components/Button.jsx
+++ basic-react-app/src/components/Button.jsx
@@ -2,10 +2,20 @@ function  printHello() {
     console.log("Hello!")
 }
 
+function  printBye() {
+    console.log("Bye!")
+}
+
+function  printClick() {
+    console.log("Double Clicked!")
+}
+
 export default function Button() {
     return (
-      <div>
-          <button onClick={printHello}>Click me!</button>
-      </div>
+        <div>
+            <button onClick={printHello}>Click me!</button>
+            <p onMouseOver={printBye}>Hover Over me!</p>
+            <button onDoubleClick={printClick}>Double Click me!</button>
+        </div>
     );
 }
