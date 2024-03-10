09aa96b2521221939700168008e772881b5b4c15
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-10 16:30:07 +0530

    re-rendering _How does it work?

--- react-state-class/src/App.jsx
+++ react-state-class/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import LikeButton from "./components/LikeButton.jsx";
+import Counter from "./components/Counter.jsx";
 
 function App() {
     return (
         <>
             <h4>State in React</h4>
-            <LikeButton />
+            <Counter />
         </>
     )
 }
--- react-state-class/src/components/Counter.jsx
+++ react-state-class/src/components/Counter.jsx
@@ -1,10 +1,13 @@
 import {useState} from "react";
 
 export default function Counter() {
-    let [count, setCount] = useState(0);
+    let [count, setCount] = useState(0);    //initialization
+    console.log('component is rendered!');
+    console.log(`count = ${count}`);
 
 let inCount = () => {
     setCount(count+1);
+    console.log(`inCount = ${count}`);
     }
 
     return (
