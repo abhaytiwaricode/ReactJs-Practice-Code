6a3e540b61d1c0d56c02f90417526a9226bdfc14
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-10 16:42:07 +0530

    callback in set state function

--- react-state-class/src/components/Counter.jsx
+++ react-state-class/src/components/Counter.jsx
@@ -2,12 +2,16 @@ import {useState} from "react";
 
 export default function Counter() {
     let [count, setCount] = useState(0);    //initialization
-    console.log('component is rendered!');
-    console.log(`count = ${count}`);
 
-let inCount = () => {
-    setCount(count+1);
-    console.log(`inCount = ${count}`);
+    let inCount = () => {
+        setCount((currCount) => {
+            return currCount + 1;
+        });
+        setCount((currCount) => {
+            return currCount + 1;
+        });
+
+        console.log(`inCount = ${count}`);
     }
 
     return (
