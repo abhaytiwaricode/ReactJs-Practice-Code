0bae10fc12fca6a4c84fc0d3520c573e38301866
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-11 14:17:52 +0530

    Unique key for list items

--- react-state-class/src/components/TodoList.jsx
+++ react-state-class/src/components/TodoList.jsx
@@ -1,11 +1,12 @@
 import {useState} from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-    let [todos, setTodos] = useState(['code']);
+    let [todos, setTodos] = useState([{task: "sample task", id: uuidv4()}]);
     let [newTodo, setNewTodo] = useState("");
 
     let addNewTask = () => {
-        setTodos([...todos, newTodo]);
+        setTodos([...todos, {task: newTodo, id: uuidv4}]);
         setNewTodo("");
     }
     let updateTodoValue = (event) => {
@@ -19,7 +20,9 @@ export default function TodoList() {
             <hr/>
             <h4>Tasks Todo</h4>
             <ul>
-                {todos.map((todo) => (<li>{todo}</li>))}
+                {todos.map((todo) => (
+                    <li key={todo.id}>{todo.task}</li>
+                ))}
             </ul>
         </div>);
 }
