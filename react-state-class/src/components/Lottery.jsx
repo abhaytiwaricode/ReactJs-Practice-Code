e09a8bf658a7f360e2b592f7feb557ff7a8e37ff
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-12 20:08:26 +0530

    Lottery Game
    Part B

--- react-state-class/src/components/Lottery.jsx
+++ react-state-class/src/components/Lottery.jsx
@@ -1,9 +1,15 @@
 import {useState} from "react";
 import "./Lottery.css";
-import {genTicket} from "../helper.js";
+import {genTicket, sum} from "../helper.js";
 
 export default function Lottery() {
     let [ticket, setTicket] = useState(genTicket(3));
+    let isWinning = sum(ticket) === 15;
+
+    let buyTicket = () => {
+        setTicket(genTicket(3));
+    }
+
     return (
         <>
             <h1>Lottery Game!</h1>
@@ -12,6 +18,9 @@ export default function Lottery() {
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
+            <br/>
+            <button onClick={buyTicket}>Buy New Ticket</button>
+            <h3>{isWinning && "Congratulations, you won!"}</h3>
         </>
     );
 }
--- react-state-class/src/helper.js
+++ react-state-class/src/helper.js
@@ -1,9 +1,13 @@
 function genTicket(n) {
     let arr = new Array(n);
-    for(let i=0; i<n; i++) {
+    for (let i = 0; i < n; i++) {
         arr[i] = Math.floor(Math.random() * 10);
     }
     return arr;
 }
 
-export {genTicket};
+function sum(arr) {
+    return arr.reduce((sum, curr) => sum + curr, 0);
+}
+
+export {genTicket, sum};
