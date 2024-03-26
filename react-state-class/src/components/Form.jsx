d193df48c40a5319bb39a55183416d25075db4ab
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-26 15:33:52 +0530

    Handling Multiple Input

--- react-state-class/src/components/Form.jsx
+++ react-state-class/src/components/Form.jsx
@@ -1,22 +1,61 @@
+import './Form.css';
 import { useState } from 'react';
 
 export default function Form() {
-  let [name, setName] = useState('');
+  let [formData, setFormData] = useState({
+    name: '',
+    username: '',
+    password: '',
+  });
 
-  let handleNameChange = (event) => {
-    setName(event.target.value);
+  let handleInputChange = (event) => {
+    setFormData((currData) => {
+      return { ...currData, [event.target.name]: event.target.value };
+    });
+  };
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    setFormData({
+      name: '',
+      username: '',
+      password: '',
+    });
+    console.log(formData);
   };
 
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="name"
-        value={name}
-        onChange={handleNameChange}
-      />
-      <br />
-      <br />
+    <form className="container" onSubmit={handleSubmit}>
+      <div className="form-input">
+        <label htmlFor="name">Name :</label> &nbsp;
+        <input
+          type="text"
+          value={formData.name}
+          onChange={handleInputChange}
+          id="name"
+          name="name"
+        />
+      </div>
+      <div className="form-input">
+        <label htmlFor="username">Username :</label> &nbsp;
+        <input
+          type="text"
+          value={formData.username}
+          onChange={handleInputChange}
+          id="username"
+          name="username"
+        />
+      </div>
+      <div className="form-input">
+        <label htmlFor="password">Password :</label> &nbsp;
+        <input
+          type="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          id="password"
+          name="password"
+        />
+      </div>
       <button>Submit</button>
     </form>
   );
