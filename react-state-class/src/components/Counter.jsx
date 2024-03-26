9b676d2e7077cd72716a49c57935b44cc57afab4
abhaytiwaricode <abhaytiwariwork@gmail.com>
2024-03-26 17:17:35 +0530

    Dependencies in useEffect( )

--- react-state-class/src/components/Counter.jsx
+++ react-state-class/src/components/Counter.jsx
@@ -1,22 +1,32 @@
 import { useState, useEffect } from 'react';
 
 export default function Counter() {
-  let [count, setCount] = useState(0);
+  let [countx, setCountx] = useState(0);
+  let [county, setCounty] = useState(0);
 
-  let inCount = () => {
-    setCount((currCount) => {
-      return currCount + 1;
-    });
+  let inCountx = () => {
+    setCountx((currCount) => currCount + 1);
   };
 
-  useEffect(function temp() {
-    console.log('this is a side-effect');
-  });
+  let inCounty = () => {
+    setCounty((currCount) => currCount + 1);
+  };
+
+  useEffect(
+    function temp() {
+      console.log('this is a side-effect');
+    },
+    [countx]
+  );
 
   return (
     <div>
-      <h3>Count = {count}</h3>
-      <button onClick={inCount}>Count +1</button>
+      <h3>Countx = {countx}</h3>
+      <button onClick={inCountx}>Count +1</button>
+      <br />
+      <br />
+      <h3>County = {county}</h3>
+      <button onClick={inCounty}>Count +1</button>
     </div>
   );
 }
